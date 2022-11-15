2b72f5940054128c0ce67a4990e7dbd66ddb1fda
Johngtka <johngtka@gmail.com>
2022-11-15 12:01:34 +0100

    .

--- script.js
+++ script.js
@@ -9,13 +9,25 @@ function kalendar() {
     if (data1.length == 0) data1 = data;
     // console.log(data1);
     var index = Math.floor(Math.random() * data1.length);
-    document.getElementById("test").innerHTML = '<p>' + data1[index] + '</p>';
+    document.getElementById("test").innerHTML = '<p>' + data1[index] + '</p>' + '<a href="#" id="back">Powrót</a>';
     data1.splice(index, 1);
     const d = document.getElementById("mydiv");
     d.classList.remove("hide");
     document.getElementById("loader").style.display = "block";
     myVar = setTimeout(showpage, 3000);
     // myvar1 = setTimeout(hide, 10000);
+    jQuery(function ($) {
+        $.scrollTo(0);
+        $('.day').click(function () {
+            $.scrollTo($('#t'), 2000);
+        });
+    });
+    jQuery(function ($) {
+        $.scrollTo(0);
+        $('#back').click(function () {
+            $.scrollTo($('#up'), 2000);
+        });
+    });
 }
 function showpage() {
     document.getElementById("loader").style.display = "none";
@@ -34,14 +46,23 @@ function hide() {
 async function czytaj() {
     return await (await fetch("tt.json")).json();
 }
+// funkcja pobierająca datę (Rok i zapisaną w tablicę nazwę miesiąca na sztywno "Grudzień")
 function time() {
+    // zmienna year ospowiada za przechowywanie obiektu klasy Date
     var year = new Date();
+    // zmienna rok odpowiada za przechowywanie wunkcji obiektu Date który pobiera pełny Rok
     var rok = year.getFullYear();
-    let monthArray = ['Grudzień'];
+    // wyrażenie year1 ściąga do skryptu element o podanym id
     const year1 = document.getElementById("date");
+    // przypisanie do wyrażenia year1 zmiennej rok która zawiera fukcje obiektu Date i 2x<br>
     year1.innerHTML = rok + '</br></br>';
+    // przypisanie wyrażeniu  year1 klasy która definiuje wygląd rego elementu wpisanego w lini 52
     year1.classList.add("rok");
+    // w zmiennej monthArray znajduje się tablica z przypisaną na sztywno wartoscią Grudzień
+    let monthArray = ['Grudzień'];
+    // przypisaniu wyrażeniu msc elementu o id msc
     const msc = document.getElementById("msc");
+    // włożenie do wyrażenia msc tablicy monthArray z indexem 0 i :
     msc.innerHTML = monthArray[0] + ":";
     // console.log(monthArray[0]);
 }
