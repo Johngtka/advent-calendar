12f450448403bdbec6d218946cce00458add5b1a
Johngtka <johngtka@gmail.com>
2022-12-09 11:46:09 +0100

    .

--- script.js
+++ script.js
@@ -15,12 +15,15 @@ function onDayClick(options) {
         alert(options.data.texts.splice(Math.round(Math.random() * options.data.texts.length), 1)[0])
     }
 }
-
 window.onload = () => {
     load().then((data) => {
         console.log(data.usedDays)
         const mainElement = document.getElementById('main')
-        if (data.date.getMonth() === 11) {
+        if (data.date.getMonth() + 1 === 12) {
+            var year = data.date.getFullYear()
+            var month = "Grudzień"
+            document.getElementById("msc").innerHTML = month
+            document.getElementById("date").innerHTML = year
             for (let i = 1; i <= 30; i++) {
                 const dayElement = document.createElement('div')
                 dayElement.classList.add('day')
@@ -37,7 +40,9 @@ window.onload = () => {
                 mainElement.appendChild(dayElement)
             }
         } else {
-            mainElement.innerText = 'Nie ma grudnia'
+            document.getElementById("interact").style.display = "none"
+            document.getElementById("info").style.display = "none"
+            mainElement.innerHTML = '<h1 style="margin:0; font-size:150px">Nie ma grudnia</h1>'
         }
     })
 }
@@ -94,27 +99,6 @@ window.onload = () => {
 //         krycie.remove();
 //     });
 // }
-// // funkcja pobierająca datę (Rok i zapisaną w tablicę nazwę miesiąca na sztywno "Grudzień")
-// function time() {
-//     // zmienna year ospowiada za przechowywanie obiektu klasy Date
-//     var year = new Date();
-//     // zmienna rok odpowiada za przechowywanie funkcji obiektu Date który pobiera pełny Rok
-//     var rok = year.getFullYear();
-//     // wyrażenie year1 ściąga do skryptu element o podanym id
-//     const year1 = document.getElementById("date");
-//     // przypisanie do wyrażenia year1 zmiennej rok która zawiera fukcje obiektu Date i 2x<br>
-//     year1.innerHTML = rok;
-//     // przypisanie wyrażeniu  year1 klasy która definiuje wygląd tego elementu wpisanego w lini 52
-//     year1.classList.add("rok");
-//     // w zmiennej monthArray znajduje się tablica z przypisaną na sztywno wartoscią Grudzień
-//     let monthArray = ['Grudzień'];
-//     // przypisaniu wyrażeniu msc elementu o id msc
-//     const msc = document.getElementById("msc");
-//     // włożenie do wyrażenia msc tablicy monthArray z indexem 0 i :
-//     msc.innerHTML = monthArray[0] + ":";
-//     // console.log(monthArray[0]);
-// }
-// //
 
 // window.onload = kalendarz;
 // function kalendarz() {
